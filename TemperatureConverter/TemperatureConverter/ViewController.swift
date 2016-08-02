c24243bdc826b64782a8421285a416bb38dc42e5
Jess <UnkJester@users.noreply.github.com>
2016-08-01 21:32:30 -0500

    Merge finished TemperatureConverter from Dev (#1)
    * Add Orange UI, button, label, and textbox to Storyboard
    
    * Finished Celsius Calculation
    
    * Added conversions counter
    
    * Updated Conversion Count Layout

--- TemperatureConverter/TemperatureConverter/ViewController.swift
+++ TemperatureConverter/TemperatureConverter/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var conversionCount = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var conversionLabel: UILabel!
+    @IBOutlet weak var tempEntry: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func convertClicked(_ sender: AnyObject) {
+        
+        // Tries to create variable from function or variable, 
+        //  and only continues if successful
+        if let result = tempEntry.text {
+            if (result == "") {
+                return
+            }
+            else {
+                if let num = Double(result) {
+                    let output = num * (9/5) + 32
+                    resultLabel.text = String(output)
+                    incrAndUpdateCount()
+                }
+            }
+        }
+    }
+    
+    func incrAndUpdateCount() {
+        conversionCount += 1;
+        conversionLabel.text = String(conversionCount) + " conversions"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
