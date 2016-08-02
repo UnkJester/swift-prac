d1cb81cad442e006b7c80fb27a9596f031c1e70c
Jess <Jesse@jesses-macbook-pro.local>
2016-08-01 21:06:11 -0500

    Finished Celsius Calculation

--- TemperatureConverter/TemperatureConverter/ViewController.swift
+++ TemperatureConverter/TemperatureConverter/ViewController.swift
@@ -15,6 +15,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var tempEntry: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func convertClicked(_ sender: AnyObject) {
+        
+        // Tries to create variable from function or variable, 
+        //  and only continues if successful
+        if let result = tempEntry.text {
+            if (result == "") {
+                return
+            }
+            else {
+                if let num = Double(result) {
+                    let output = num * (9/5) + 32
+                    resultLabel.text = String(output)
+                }
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
