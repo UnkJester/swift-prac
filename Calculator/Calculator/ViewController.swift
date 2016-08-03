064daa991448a0d61a142d4dad9d5b3eb5704816
Jess <Jesse@jesses-macbook-pro.local>
2016-08-02 20:27:46 -0500

    Added Actions and linked buttons

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -14,6 +14,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // Button Actions
+    @IBAction func numberClicked(_ sender: UIButton) {
+        print(String(sender.tag))
+    }
+    @IBAction func operatorClicked(_ sender: UIButton) {
+        switch sender.tag {
+        case 10:
+            print("add")
+        case 11:
+            print("minus")
+        case 12:
+            print("times")
+        case 13:
+            print("divide")
+        default:
+            return
+        }
+    }
+    @IBAction func equalsClicked(_ sender: UIButton) {
+        print("equals")
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
